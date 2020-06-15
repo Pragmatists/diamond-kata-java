67f6f41a6bf32d4e03ba45cd88d00aa5c06a22d7
michal.lipski <kiwaczki@gmail.com>
2020-06-15 16:31:53 +0200

    initial

--- src/main/java/com/pragmatists/Diamond.java
+++ src/main/java/com/pragmatists/Diamond.java
@@ -2,12 +2,4 @@ package com.pragmatists;
 
 public class Diamond {
 
-    public static String print(char letter) {
-        int diamondSize = letter - 'A';
-        return spaces(diamondSize) + "A" + spaces(diamondSize);
-    }
-
-    private static String spaces(int count) {
-        return "-".repeat(count);
-    }
 }
--- src/test/java/com/pragmatists/DiamondShould.java
+++ src/test/java/com/pragmatists/DiamondShould.java
@@ -7,28 +7,4 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class DiamondShould {
 
-    @Test
-    void format_first_line_for_A() {
-        String result = Diamond.print('A');
-
-        assertThat(result).isEqualTo("A");
-    }
-
-    @Test
-    void format_first_line_for_B() {
-        String result = Diamond.print('B');
-        assertThat(result).isEqualTo("-A-");
-    }
-
-    @Test
-    void format_first_line_for_C() {
-        String result = Diamond.print('C');
-        assertThat(result).isEqualTo("--A--");
-    }
-
-    @Test
-    void format_first_line_for_D() {
-        String result = Diamond.print('D');
-        assertThat(result).isEqualTo("---A---");
-    }
 }
